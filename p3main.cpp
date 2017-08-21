9d44552c53ffe056379c9edeec4cdd910068b4cf
Jiwon Jeon <jiwon.jeon520@gmail.com>
2017-08-21 00:44:58 -0500

    Update p3main.cpp

--- p3main.cpp
+++ p3main.cpp
@@ -5,63 +5,6 @@
 
 using namespace std;
 
-/*void printBits(unsigned char s) {
-	//print the bit pattern
-	for (int i = 0; i < 8; i++)
-		if (s & (1 << (7 - i)))
-			cout << 1;
-		else
-			cout << 0;
-}*/
-
-/*int main() {
-	int n = 10;
-	int size;
-	// Create the bit Adjacency Matrix structure
-	if (n % 8 == 0)
-		size = n / 8;
-	else
-		size = (int)(n / 8) + 1;
-	unsigned char** bitAdjacencyMatrix;
-	bitAdjacencyMatrix = new unsigned char*[n];
-	for (int i = 0; i < n; i++) {
-		bitAdjacencyMatrix[i] = new unsigned char[size];
-	}
-	//Initialize the bit Adjacency Matrix structure
-	for (int i = 0; i < n; i++){
-		for (int j = 0; j < size; j++)
-			bitAdjacencyMatrix[i][j] = 0x00 << 8;
-	}
-	//Print the bit Adjacency Matrix structure
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < size; j++)
-			printBits(bitAdjacencyMatrix[i][j]);
-		cout << i << endl;
-	}
-}*/
-
-/*int main() {
-	unsigned char l = 0x01 << 1; // set l to be equal to 2;
-	cout << "Printing the bits before changing: ";
-	printBits(l);
-	cout << endl;
-	//set the third bit from right to be a 1
-	l |= 0x01 << 2;
-	cout << "Printing the bits after setting the third bit from the right to be a 1: ";
-	printBits(l);
-	cout << endl;
-	//set the second bit from the right to be a 0
-	l &= ~(0x01 << 1);
-	cout << "Printing the bits after setting the second bit from the right to be a 0: ";
-	printBits(l);
-	cout << endl;
-	//check the second bit from the right
-	if (l & (1 << 1))
-		cout << "Second bit from right is set" << endl;
-	else
-		cout << "Second bit from right is not set" << endl;
-}*/
-
 int main() {
 	int node;
 	int vertexU, vertexV;
